fa2cd86948869d63e8a7089bd75e26c302de85a5
Ken <kcolon018@gmail.com>
2018-09-26 12:16:02 -0400

    Initial commit

---
+++ Training02.py
@@ -0,0 +1,18 @@
+#THIS IS THE CONTINUATION OF TRAINING01 WITH ALL THE CODE SUMMARIZED IN ONE FUNCTION
+
+from __future__ import print_function
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+
+
